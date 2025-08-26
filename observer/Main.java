bf656ea61646d51b8a2e7cb5c1bb0dc841cdd0a2
topcualperen <alperenttpc78@gmail.com>
2025-08-26 18:18:12 +0300

    implement Observer design pattern with User and NewsAgency classes

---
+++ observer/Main.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
+interface Observer {
+	void update(String message);
+}
+
+class User implements Observer {
+	private String name;
+
+	User(String name) {
+		this.name = name;
+	}
+
+	public void update(String message) {
+		System.out.println("Message send to " + name + " : " + message);
+	}
+}
+
+class NewsAgency {
+	private List<Observer> observers = new ArrayList<>();
+	private String news;
+
+	public void addObserver(Observer observer) {
+		observers.add(observer);
+	}
+
+	public void removeObserver(Observer observer) {
+		if (observers.contains(observer)) {
+			observers.remove(observer);
+		}else {
+			throw new IllegalArgumentException("Not Found !!!");
+		}
+	}
+
+	public void sendNews(String news) {
+		this.news = news;
+		notifyAllObervers();
+	}
+
+	private void notifyAllObervers() {
+		for (Observer observer : observers) {
+			observer.update(news);
+		}
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		User user = new User("Jack");
+		User user2 = new User("Baki");
+		User user3 = new User("Yujiro");
+
+		NewsAgency newsAgency = new NewsAgency();
+		newsAgency.addObserver(user);
+		newsAgency.addObserver(user2);
+		newsAgency.addObserver(user3);
+
+		newsAgency.sendNews("Hello my friends");
+	}
+}
